0514cf8b1f3099a31382b3e7ff0515c8d4b0dc93
fruizga <ruizgallego@gmail.com>
2021-03-15 12:15:24 -0500

    Update Gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_07_140656) do
+ActiveRecord::Schema.define(version: 2021_03_08_172246) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
   end
 
