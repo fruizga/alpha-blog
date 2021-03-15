bc8b5d6d5d595a981e08d0cb17308e4b7bb43b9a
fruizga <ruizgallego@gmail.com>
2021-03-15 10:46:28 -0500

    Add sessions create and destroy for logging users in and out

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,15 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
+   def index
+    @articles = Article.paginate(page: params[:page], per_page: 5)
+  end
+
   def show
     
   end
 
-  def index
-    @articles = Article.paginate(page: params[:page], per_page: 5)
-  end
-
-  def new
+ def new
     @article = Article.new
   end
 
